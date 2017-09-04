fb17270875b512595f6b915b591a00b8ca8c8330
defe266 <gabriel@gabrielgomez.es>
2017-09-04 09:55:54 +0200

    image stream error handler

--- routes/helpers/imageTransforms.js
+++ routes/helpers/imageTransforms.js
@@ -1,22 +1,8 @@
 var imgSteamHandler = require('../../lib/imgSteamHandler.js');
-var path = require('path');
-var fs = require('fs');
 
 
 module.exports = function(req, res, next){
 
-	var file = req.url.split('/:/')[0];
-	var filePath = path.resolve(__dirname, '../../public'+file);
-
-	//console.log('1',filePath);
-
-	//fs.exists(file, function(exists) {
-
-	fs.access(filePath,function(err){
-
-		if (err) return next();
-
-		imgSteamHandler(req,res);
-	});
+	imgSteamHandler(req,res);
 
 }
